c54c2d23beaef79ebacf6d318f51f8ec465ef654
晓杰 <2561589453@qq.com>
2023-03-23 22:04:08 +0800

    add  log

--- KubeArmor/core/containerdHandler.go
+++ KubeArmor/core/containerdHandler.go
@@ -133,7 +133,7 @@ func (ch *ContainerdHandler) GetContainerInfo(ctx context.Context, containerID s
 
 	container.ContainerID = res.Container.ID
 	container.ContainerName = res.Container.ID[:12]
-	fmt.Printf("res.Container.ID:%+v\n", res.Container.ID)
+	fmt.Printf("res.Container.ID:%+v， container name:%v\n", res.Container.ID, container.ContainerName)
 	container.NamespaceName = "Unknown"
 	container.EndPointName = "Unknown"
 
